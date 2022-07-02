cb27847b640c4dc58b98d2bd65507570b3d5ce67
Daniel Elkoni <44883449+Fallvirus009@users.noreply.github.com>
2022-07-01 21:57:03 -0400

    Update SmartSpellCheck.py

--- SmartSpellCheck.py
+++ SmartSpellCheck.py
@@ -1,20 +1,26 @@
-def spellCheck(word, key, accPercForCorr):
+def spellCheck(word, key, accPercForCorr, caseSens):
     accuracy = 0
     total = len(key)
+    if caseSens == False:
+        w = word.lower()
+        k = key.lower()
+    else:
+        w = w
+        k = key
 
-    if len(word) == len(key):
-        print(f"+1 acc {len(word)}={len(key)}")
+    if len(w) == len(k):
+        print(f"+1 acc {len(w)}={len(k)}")
         accuracy += 1
 
-    for i in range(len(word)):
-        if word[i] == key[i]:
+    for i in range(len(w)):
+        if w[i] == k[i]:
             accuracy += 1
-            print(f"+1 acc {word[i]}{key[i]}")
+            print(f"+1 acc {w[i]}{k[i]}")
         i += 1
 
     if round(accuracy/total,2) >= (accPercForCorr/100):
-        print(f'{round(accuracy/total,2)} Same word')
+        print(f'{round(accuracy/total,2)} Same w')
     else:
         print(f'{round(accuracy/total,2)} Not same')
 
-spellCheck('wammsbe', 'wannabe', 70)
+spellCheck('wammsbe', 'wannabe', 70, False)
