7d735bc09d4865ec65640f299fdea354a38c1178
Daniel Elkoni <44883449+Fallvirus009@users.noreply.github.com>
2022-07-02 00:05:59 -0400

    Update SmartSpellCheck.py

--- SmartSpellCheck.py
+++ SmartSpellCheck.py
@@ -1,3 +1,6 @@
+def ins(a,b,c):
+    return a[:c]+b+a[c:]
+
 def spellCheck(word, key, accPercForCorr, caseSens):
     accuracy = 0
     total = len(key)
@@ -8,19 +11,22 @@ def spellCheck(word, key, accPercForCorr, caseSens):
         w = w
         k = key
 
-    if len(w) == len(k):
-        print(f"+1 acc {len(w)}={len(k)}")
-        accuracy += 1
-
     for i in range(len(w)):
         if w[i] == k[i]:
             accuracy += 1
             print(f"+1 acc {w[i]}{k[i]}")
+        if w[i] == ' ':
+            if k[i] != ' ':
+                k = ins(k,' ',i)
         i += 1
+    
+    if len(w) == len(k):
+        print(f"+1 acc {len(w)}={len(k)}")
+        accuracy += 1
 
     if round(accuracy/total,2) >= (accPercForCorr/100):
-        print(f'{round(accuracy/total,2)} Same w')
+        print(f'{round(accuracy/total,2)} Same word/phrase')
     else:
-        print(f'{round(accuracy/total,2)} Not same')
+        print(f'{round(accuracy/total,2)} Not same word/phrase')
 
-spellCheck('wammsbe', 'wannabe', 70, False)
+spellCheck('wamms be cool', 'wannabe cool', 70, False)
