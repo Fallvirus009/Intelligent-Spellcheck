ec1411587fc856d61d8a6bf53b04148ee7651d10
Daniel Elkoni <44883449+Fallvirus009@users.noreply.github.com>
2022-07-01 21:22:36 -0400

    Add files via upload

---
+++ SmartSpellCheck.py
@@ -0,0 +1,20 @@
+def spellCheck(word, key):
+    accuracy = 0
+    total = len(key)
+
+    if len(word) == len(key):
+        print(f"+1 acc {len(word)}={len(key)}")
+        accuracy += 1
+
+    for i in range(len(word)):
+        if word[i] == key[i]:
+            accuracy += 1
+            print(f"+1 acc {word[i]}{key[i]}")
+        i += 1
+
+    if round(accuracy/total,2) >= .70:
+        print(f'{round(accuracy/total,2)} Same word')
+    else:
+        print(f'{round(accuracy/total,2)} Not same')
+
+spellCheck('wammsbe', 'wannabe')
